c129656797dcbc42ddd0f06d71be9d8cd5990a23
viladrich24 <arnau.viladrich@alu-etsetb.upc.edu>
2017-06-28 15:11:19 +0200

    Hola

---
+++ app/src/main/java/viladrich/arnau/pistoler/banderes.java
@@ -0,0 +1,13 @@
+package viladrich.arnau.pistoler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class banderes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banderes);
+    }
+}
